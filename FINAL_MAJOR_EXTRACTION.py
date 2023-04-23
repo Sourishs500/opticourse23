83d4d912027206636fda7652c7d2dedf80738f1d
Sparsh Johri <bhatabhaiyya@gmail.com>
2023-04-23 01:28:31 -0700

    Major Extraction Portions Combined Into A Single Program

--- FINAL_MAJOR_EXTRACTION.py
+++ FINAL_MAJOR_EXTRACTION.py
@@ -1,24 +1,56 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
+from bs4 import BeautifulSoup
+import requests
 from word2number.w2n import word_to_num
 
 
-filename = 'Computer_Engineering_B.S._DegreeRequirements.txt'
+major = "Computer Science and Engineering B.S."#"Electrical Engineering B.S." #input
 
+
+
+baseURL = "https://catalog.registrar.ucla.edu/major/2022/"
+fullURL = baseURL + "".join([i for i in major if i.isalnum()])
+driver = webdriver.Chrome('chromedriver.exe') 
+driver.get(fullURL)
+time.sleep(0.5)
+
+
+majorInfo = driver.find_elements(By.XPATH, "//div[starts-with(@class, 'main-content')]")
+print(majorInfo[0].get_attribute("class"), "\n\n\n")
+
+majorInfo = majorInfo[0].find_elements(By.XPATH, "./child::*")
+majorInfo = ([BeautifulSoup(i.get_attribute("innerHTML"), "html.parser") for i in majorInfo])#get_attribute("name")
+
+ACTUAL = majorInfo[3]
+
+
+driver.quit()
+
+A = BeautifulSoup(ACTUAL.prettify().replace("span", "br").replace("keyboard_arrow_down", "").replace("Expand all", ""), 
+                  "html.parser").text
+for i in range(3):
+    A = A.replace("\n\n", "\n")#.replace("\n\n", "\n").replace("\n\n", "\n").replace("\n\n", "\n")
+
+file = A.split("\n")
 
 # File to list of lines
 lines = []
 lines_extras = []
 extrasDetect = False
-with open(filename) as file:
-    for line in file:
-        # After first instance of extras like honors in text, rest of text goes to extras list instead of normal list
-        if extrasDetect == False and ('Honors' in line):
-            extrasDetect = True
-        # Include line if not empty and contains keywords
-        if (line.rstrip() != '') and ('Complete' in line or 'course' in line or ('Select' in line and 'from' in line) or line.lstrip()[1].isupper() or line.lstrip()[1] == '&' or line.lstrip()[1] == ' '):
-            if not extrasDetect:
-                lines.append(line.rstrip().split(' - ', 1)[0]) # split: remove text after course number
-            else:
-                lines_extras.append(line.rstrip().split(' - ', 1)[0]) # split: remove text after course number
+for line in file:
+    # After first instance of extras like honors in text, rest of text goes to extras list instead of normal list
+    if extrasDetect == False and ('Honors' in line):
+        extrasDetect = True
+    # Include line if not empty and contains keywords
+    if (line.rstrip() != '') and ('Complete' in line or 'course' in line or ('Select' in line and 'from' in line) or line.lstrip()[1].isupper() or line.lstrip()[1] == '&' or line.lstrip()[1] == ' '):
+        if not extrasDetect:
+            lines.append(line.rstrip().split(' - ', 1)[0]) # split: remove text after course number
+        else:
+            lines_extras.append(line.rstrip().split(' - ', 1)[0]) # split: remove text after course number
 
 # Find first instance of a number in a list and return it
 def find_first_num(list):

be32f0d80c52af3074992f895cc7851598bf98d4
Sourishs500 <31145059+Sourishs500@users.noreply.github.com>
2023-04-23 07:03:09 -0700

    Update script.js

--- LAHACKS2023/templates/script.js
+++ LAHACKS2023/templates/script.js
@@ -126,4 +126,25 @@ function sendToFlask() {
 
   //  fetch API call
 
+
+function checkForm() {
+  const elements = document.querySelectorAll('.req');
+
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].value === "") {
+      alert("Enter a value into every field!");
+      return false;
+    }
+  }
+
+  return checkQuarters();
+}
+
+function checkQuarters() {
+  var num = document.getElementById("quarter-select").value;
+    if (isNaN(num) || num < 9 || num > 15) {
+      alert("Enter a number of quarters!");
+      return false;
+    }
+  return true;
 }
